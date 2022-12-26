f0ba1cf4d2a615551b1d269a842b359ef82cc36c
Nastya_krevtka <nastyamkala1278@gmail.com>
2022-12-26 14:09:37 +0300

    задача1 четные и нечетные элементы

---
+++ ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+int ArraySize = 6;
+int CheckEven = 0;
+int CheckUneven =0;
+int[] array = new int[ArraySize];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(100, 1000);
+
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        CheckEven++;
+    }
+    else
+    {
+        CheckUneven++;
+    }
+}
+Console.WriteLine("количество четных элементов: " + CheckEven);
+Console.WriteLine("количество нечетных элементов: " + CheckUneven);
